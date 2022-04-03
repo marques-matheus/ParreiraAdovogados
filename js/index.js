f3ffb2e9ef2f5ec7ba01a18fbf103f2d89a3f9a1
Matheus Marques <marques.m4th3u5@gmail.com>
2022-04-03 03:24:09 -0300

    Finalização do rodape, refatoração do cabeçalho

--- js/index.js
+++ js/index.js
@@ -1,18 +1,26 @@
 const $img = document.querySelector(".logo");
 const $nav = document.querySelector(".nav__conteudo");
+const $link = document.querySelectorAll(".nav__link");
 
 window.addEventListener("scroll", () => {
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
     $nav.style =
-      "background-color: white; opacity: 0.8; position: fixed; top: 0;  width: 100%;";
+      "background-color: white; opacity: 0.9; position: fixed; top: 0;";
     $img.src = "../img/LOGOPreto.png";
+    $link.forEach((elemento) => {
+      elemento.classList.add("nav__link--preto");
+    });
   } else if (
     document.body.scrollTop < 60 ||
     document.documentElement.scrollTop < 60
   ) {
-    $nav.style =
-      "background-color: #3f3f3f; position: fixed; top: 0;  width: 100%;";
+    $nav.style = "background-color: #3f3f3f; position: fixed; top: 0;";
     $img.src = "../img/LOGOColorida.png";
+    $link.forEach((elemento) => {
+      elemento.classList.remove("nav__link--preto");
+      elemento.classList.add("nav__link--branco");
+    });
+    
   }
 });
 
@@ -24,7 +32,8 @@ let clicado = false;
 $botao.addEventListener("click", () => {
   if (clicado === false) {
     clicado = true;
-    $lista.style = "visibility: visible; opacity:1; transition: opacity 300ms ease-in;";
+    $lista.style =
+      "visibility: visible; opacity:1; transition: opacity 300ms ease-in;";
   } else if (clicado === true) {
     clicado = false;
     $lista.style = "transition: opacity 300ms ease-out";
