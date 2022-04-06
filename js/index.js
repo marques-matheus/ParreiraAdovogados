e402c4e42f4f2ac870595dd600e247369c686583
Matheus Marques <marques.m4th3u5@gmail.com>
2022-04-05 23:25:57 -0300

    Troca das imagens por icones e update da  pagina de areas

--- js/index.js
+++ js/index.js
@@ -2,26 +2,26 @@ const $img = document.querySelector(".logo");
 const $nav = document.querySelector(".nav__conteudo");
 const $link = document.querySelectorAll(".nav__link");
 
-// window.addEventListener("scroll", () => {
-//   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-//     $nav.style =
-//       "background-color: white; opacity: 0.9; position: fixed; top: 0;";
-//     $img.src = "../img/LOGOPreto.png";
-//     $link.forEach((elemento) => {
-//       elemento.classList.add("nav__link--preto");
-//     });
-//   } else if (
-//     document.body.scrollTop < 60 ||
-//     document.documentElement.scrollTop < 60
-//   ) {
-//     $nav.style = "background-color: #3f3f3f; position: fixed; top: 0;";
-//     $img.src = "../img/LOGOColorida.png";
-//     $link.forEach((elemento) => {
-//       elemento.classList.remove("nav__link--preto");
-//       elemento.classList.add("nav__link--branco");
-//     });
-//   }
-// });
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    $nav.style =
+      "background-color: white; opacity: 0.9; position: fixed; top: 0;";
+    $img.src = "../img/LOGOPreto.png";
+    $link.forEach((elemento) => {
+      elemento.classList.add("nav__link--preto");
+    });
+  } else if (
+    document.body.scrollTop < 60 ||
+    document.documentElement.scrollTop < 60
+  ) {
+    $nav.style = "background-color: #3f3f3f; position: fixed; top: 0;";
+    $img.src = "../img/LOGOColorida.png";
+    $link.forEach((elemento) => {
+      elemento.classList.remove("nav__link--preto");
+      elemento.classList.add("nav__link--branco");
+    });
+  }
+});
 
 const $botao = document.querySelector(".btn__menu");
 const $lista = document.querySelector(".nav__lista--mobile");
